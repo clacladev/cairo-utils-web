275f49ad38cd06bbee9535caf0e9e67e94e9c0b9
Claudio Carnino <claudio@tugulab.org>
2022-03-09 15:31:23 +0000

    Updated the footer

--- modules/layout/components/Footer.tsx
+++ modules/layout/components/Footer.tsx
@@ -1,14 +1,15 @@
+import Link from "next/link";
+
 export default function Footer() {
   return (
     <div id="footer" className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-16 mb-16 text-sm text-gray-600">
       <div className="border-t py-5 border-gray-200" />
-      <span className="block font-bold">Disclaimer</span>
+      
       <span className="block">
-        Use this website/project at your own peril and risk. The project comes with no guarantees of any kind. Do not keep me accountable.
+        Created by <Link href="https://twitter.com/ccarnino">clacla.eth</Link>. View source code on <Link href="https://github.com/ccarnino/cairo-utils-web">Github</Link>.
       </span>
-
-      <span className="block font-bold pt-3">Acknowledgments</span>
-      <span className="block">
+      
+      <span className="block pt-3">
         Thanks to <a href="https://github.com/sekai-studio/starknet-libs">sekai-studio/starknet-libs</a> for their cairo_string library.
       </span>
     </div>
